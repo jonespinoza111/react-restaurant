0a9c71727810272ef129827d51804a534461f9d8
jonespinoza111 <jonespinoza111@gmail.com>
2023-07-25 12:38:13 -0400

    small changes

--- src/components/MenuMain.jsx
+++ src/components/MenuMain.jsx
@@ -12,7 +12,7 @@ const MenuMain = () => {
       <section className="meal-card-section w-[100%] flex flex-col justify-center items-center">
         <div className="my-10 uppercase">Menu</div>
         <Menu setCurrentCat={setCurrentCat}  />
-        <div className="meal-cards-container w-[90%] flex flex-col items-center lg:flex-row lg:flex-wrap lg:justify-center mb-10">
+        <div className="meal-cards-container w-[95%] flex flex-col items-center lg:flex-row lg:flex-wrap lg:justify-center mb-10">
           {menuItems
             .filter((mealInfo) => mealInfo.category === currentCat)
             .map((mealInfo, index) => (
